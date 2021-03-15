6e53e915bde2fb973823f3b14a204468307e81db
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-03-15 13:51:07 +1030

    developed presentation

---
+++ getPackages.jl
@@ -0,0 +1,16 @@
+## import("pathto/getPackages.jl")
+## run the line above in the julia REPL execute
+## install packages
+import Pkg 
+Pkg.add("DataFrames") # similar to dplyr, i think
+Pkg.add("CSV") # to read in CSV files
+Pkg.add("Plots") # 
+Pkg.add("Distributions") # julia equivalent of pnorm etc
+Pkg.add("HypothesisTests") # julia equivalents of t.test etc
+Pkg.add("GLM") # the linear models package
+Pkg.add("RDatasets") # 700+ datasets from R packages
+
+## save some data to read in
+import RDatasets, CSV
+cars = RDatasets.dataset("Ecdat","CPSch3")
+CSV.write("income.csv",cars)
